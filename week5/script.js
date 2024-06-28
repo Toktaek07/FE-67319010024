1af05974633224eb586bcb9f3572c9fcb5f4bbf0
Toktaek07 <67319010024@udontech.ac.th>
2024-06-28 17:12:50 +0700

    Add files via upload

---
+++ week5/script.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+
+    <h1>หัวข้อใหญ่</h1>
+    <h1>หัวข้อย่อย1</h1>
+    <h2>หัวข้อย่อย2</h2>
+    <h3>หัวข้อย่อย3</h3>
+ 
+    <p>ส่วนเนื้อหา</p>
+    <p>เนื้อหาส่วนย่อย</p> 
+     
+    <a href="http://www.mindphp.com/">ลิงค์</a> 
+
+    <img src="/w3schools.jpg" width="104" height="142"> 
+
+    <p>The hr tag defines a horizontal rule:</p>
+    <hr><p>This is a paragraph</p>
+    <hr><p>This is a paragraph</p>
+    <hr><p>This is a paragraph</p>
+
+    <!--This comment will not be displayed-->
+<p>This is a regular paragraph</p>
+</body>
+</html>
